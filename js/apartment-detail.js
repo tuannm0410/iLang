4052d64f5603703eb42bfe7e7880e340f5d1fee9
tuannm <tuannm@sapo.vn>
2020-04-04 22:46:39 +0700

    Update

--- js/apartment-detail.js
+++ js/apartment-detail.js
@@ -1,24 +1,18 @@
-var inputs = document.querySelectorAll( '.inputfile' );
-Array.prototype.forEach.call( inputs, function( input )
-{
-  var label	 = input.nextElementSibling,
-    labelVal = label.innerHTML;
-
-  input.addEventListener( 'change', function( e )
-  {
-    var fileName = '';
-    if( this.files && this.files.length > 1 )
-      fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
-    else
-      fileName = e.target.value.split( '\\' ).pop()
-
-    if( fileName )
-      label.querySelector('span').innerHTML = fileName;
-    else
-      label.innerHTML = labelVal;
-  });
-});
-
 $(function() {
   $('#receiver').tagsinput();
+
+  $('.inputfile').change(function(e) {
+    alert(e.target.value.split( '\\' ).pop());
+
+    if( $(this).files && $(this).files.length > 1 ) {
+      fileName = ( $(this).getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', e.files.length );
+    } else {
+      fileName = e.target.value.split( '\\' ).pop()
+    }
+
+    if(fileName)
+      $('.attachment span').html(fileName);
+    else
+    $('.attachment span').html("Đính kèm file");
+  });
 });
